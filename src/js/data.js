c227bd0eb8e804d2c6858de7e2a34a155da8a930
NtombekaziSibetyu <sntombekazi@gmail.com>
2020-10-15 13:11:52 +0200

    update website

--- src/js/data.js
+++ src/js/data.js
@@ -6,7 +6,7 @@ let places = [
     location: "Paarl",
     price: 1000,
     bedrooms: 1,
-    bedrooms: 1,
+   
     status: "For rent",
   },
   {
@@ -15,7 +15,7 @@ let places = [
     location: "Constantia",
     price: 2500,
     bedrooms: 2,
-    bedrooms: 2,
+    bathrooms: 2,
     status: "For sale",
   },
   {
@@ -24,7 +24,7 @@ let places = [
     location: "Rondebosch",
     price: 1500,
     bedrooms: 3,
-    bedrooms: 2,
+    bathrooms: 1,
     status: "For rent",
   },
   {
@@ -33,19 +33,21 @@ let places = [
     location: "Camps Bay",
     price: 5000,
     bedrooms: 4,
+    bathrooms: 3,
     status: "For sale",
   },
   {
-    image: "",
+    image: 'https://www.mibor.com/clientuploads/_photos/_6/pexels-photo-1396122.jpeg',
+
     type: "Apartment",
     location: "Paarl",
     price: 3500,
-    bedrooms: 4,
-    baths : 2,
+    bedrooms: 3,
+    bathrooms: 2,
     status: "For sale",
   },
   {
-    image: "",
+    image: "https://www.investopedia.com/thmb/7GOsX_NmY3KrIYoZPWOu6SldNFI=/735x0/houses_and_land-5bfc3326c9e77c0051812eb3.jpg",
     type: "Estate House",
     location: "Constantia",
     price: 4000,
@@ -54,7 +56,7 @@ let places = [
     status: "For rent",
   },
   {
-    image: "",
+    image: "https://www.investopedia.com/thmb/7GOsX_NmY3KrIYoZPWOu6SldNFI=/735x0/houses_and_land-5bfc3326c9e77c0051812eb3.jpg",
     type: "Condo",
     location: "Rondebosch",
     price: 1000,
@@ -63,7 +65,7 @@ let places = [
     status: "For sale",
   },
   {
-    image: "",
+    image: "https://lid.zoocdn.com/645/430/c7a891fc78c1eda67433cd5782ff989f7e7e9d37.jpg",
     type: "Villa",
     location: "Camps Bay",
     price: 4500,
@@ -71,7 +73,17 @@ let places = [
     baths : 1,
     status: "For rent",
   },
+  {
+    image: "https://www.investopedia.com/thmb/7GOsX_NmY3KrIYoZPWOu6SldNFI=/735x0/houses_and_land-5bfc3326c9e77c0051812eb3.jpg",
+    type: "Villa",
+    location: "Camps Bay",
+    price: 5500,
+    bedrooms: 1,
+    baths : 1,
+    status: "For rent",
+  },
 ];
+
 
 function search() {
 
@@ -94,39 +106,41 @@ function search() {
     }
     
   });
+  //when the search does not match any property in the data
   console.log(myPlaces);
   if(myPlaces.length === 0){
-    card += `<h2> No match Found</h2>`
+    card += `<h2 class="text-center"> Search does not match any property</h2>`
   }
   // the card
   
   for (let i = 0; i < myPlaces.length; i++) {
     card += `<div class="card">
-        <div class="card-header">
+      <div >
+        
         <img class="card-img" src=${myPlaces[i].image} alt="property-img">
-        </div>
+        <span class="card-img-overlay">${myPlaces[i].status}</span>
+      </div>
         <div class="card-content">
             <h3>${myPlaces[i].type}</h3>
-            <p>
-            
-            ${myPlaces[i].location}</p>
-            <p>Price : R${myPlaces[i].price}
-            <span>${myPlaces[i].status}</span>
+            <ul>
+              <li>${myPlaces[i].location}</li>
+              <li>
+              <li>Price : R${myPlaces[i].price}</li>
+            </ul>
         </div>
-        <hr>
         <div class="card-footer">
             <ul>
+            </li>
                 <li>
-                    ${myPlaces[i].bedrooms} Bedrooms
+                    ${myPlaces[i].bedrooms} Bedroom
                 </li>
                 <li>
-                    ${myPlaces[i].baths} Bedrooms
+                    ${myPlaces[i].baths} Bathroom
                 </li>
-            </ul>
-           
+            </ul>   
         </div>
     </div>`;
   }
-  document.getElementById("properties-slider").innerHTML = card;
+  document.getElementById("properties").innerHTML = card;
 };
 
